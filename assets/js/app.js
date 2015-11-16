d582c9af31aa6505ebc7bb17cd953b849bef1acd
Evin Revello <Evin@EvinRevello.com>
2015-11-15 22:35:41 -0800

    polishing it up

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,44 +12,48 @@ function renderData() {
     document.getElementsByClassName('phone')[0].innerHTML = data.phone;
     document.getElementsByClassName('location')[0].innerHTML = data.location;
 
-
-    document.getElementsByClassName('job-title-1')[0].innerHTML = data.jobs[0].position;  
-    document.getElementsByClassName('job-logo-1')[0].innerHTML = data.jobs[0].company;  
-    document.getElementsByClassName('job-start-end-1')[0].innerHTML = data.jobs[0].start.substring(0, 4) + " to " + data.jobs[0].end;  //Needs logic to handle Present case  
-    document.getElementsByClassName('description-job-1')[0].innerHTML = data.jobs[0].description; 
-
-    document.getElementsByClassName('job-title-2')[0].innerHTML = data.jobs[1].position;  
-    document.getElementsByClassName('job-logo-2')[0].innerHTML = data.jobs[1].company;  
-    document.getElementsByClassName('job-start-end-2')[0].innerHTML = data.jobs[1].start.substring(0, 4) + " to " + data.jobs[1].end.substring(0, 4);    
-    document.getElementsByClassName('description-job-2')[0].innerHTML = data.jobs[1].description; 
-
-    document.getElementsByClassName('job-title-3')[0].innerHTML = data.jobs[2].position;  
-    document.getElementsByClassName('job-logo-3')[0].innerHTML = data.jobs[2].company;  
-    document.getElementsByClassName('job-start-end-3')[0].innerHTML = data.jobs[2].start.substring(0, 4) + " to " + data.jobs[2].end.substring(0, 4);    
-    document.getElementsByClassName('description-job-3')[0].innerHTML = data.jobs[2].description; 
-
-    document.getElementsByClassName('job-title-4')[0].innerHTML = data.jobs[3].position;  
-    document.getElementsByClassName('job-logo-4')[0].innerHTML = data.jobs[3].company;  
-    document.getElementsByClassName('job-start-end-4')[0].innerHTML = data.jobs[3].start.substring(0, 4) + " to " + data.jobs[3].end.substring(0, 4);    
-    document.getElementsByClassName('description-job-4')[0].innerHTML = data.jobs[3].description; 
-
-
-    document.getElementsByClassName('testimonial-description-1')[0].innerHTML = data.testimonials[0].quote;  
-    document.getElementsByClassName('testimonial-name-1')[0].innerHTML = data.testimonials[0].name;  
-    document.getElementsByClassName('testimonial-relation-1')[0].innerHTML = data.testimonials[0].relation;    
-
-    document.getElementsByClassName('testimonial-description-2')[0].innerHTML = data.testimonials[1].quote;  
-    document.getElementsByClassName('testimonial-name-2')[0].innerHTML = data.testimonials[1].name;  
-    document.getElementsByClassName('testimonial-relation-2')[0].innerHTML = data.testimonials[1].relation;   
-
-    document.getElementsByClassName('testimonial-description-3')[0].innerHTML = data.testimonials[2].quote;  
-    document.getElementsByClassName('testimonial-name-3')[0].innerHTML = data.testimonials[2].name;  
-    document.getElementsByClassName('testimonial-relation-3')[0].innerHTML = data.testimonials[2].relation;  
+    
+    for (i=0; i < data.jobs.length; i++){
+        console.log('adsf' + i)
+        document.getElementsByClassName('job-title')[i].innerHTML = data.jobs[i].position; 
+        document.getElementsByClassName('job-start-end')[i].innerHTML = data.jobs[i].start.substring(0, 4) + " to " + (data.jobs[i].end ? data.jobs[i].end.substring(0, 4): 'Present');  //Needs logic to handle Present case  
+        document.getElementsByClassName('job-description')[i].innerHTML = data.jobs[i].description; 
+        if (data.jobs[i].logo) {
+            document.getElementsByClassName('job-center')[i].style.backgroundImage = 'url(' + data.jobs[i].logo + ')';
+            document.getElementsByClassName('job-center')[i].className += ' job-logo';
+            document.getElementsByClassName('job-center')[i].innerHTML = '';
+        }
+        else {
+            document.getElementsByClassName('job-center')[i].innerHTML = data.jobs[i].company; 
+            document.getElementsByClassName('job-center')[i].className += ' job-company'; 
+        }
+    }
+ 
+    for (i=0; i < data.testimonials.length; i++)  {
+        document.getElementsByClassName('testimonial-description')[i].innerHTML = data.testimonials[i].quote;  
+        document.getElementsByClassName('testimonial-name')[i].innerHTML = data.testimonials[i].name;  
+        document.getElementsByClassName('testimonial-relation')[i].innerHTML = data.testimonials[i].relation; 
+    }
 
 
-    document.getElementsByClassName('school-name')[0].innerHTML = data.education[0].school;  
-    document.getElementsByClassName('field')[0].innerHTML = data.education[0].degrees[0].name;  
-    document.getElementsByClassName('year')[0].innerHTML = data.education[0].degrees[0].date;    
+
+    for (k=0; k < data.education.length; k++) {
+        if (data.education[k].logo) {
+            debugger;
+            document.getElementsByClassName('job-center')[i].style.backgroundImage = 'url(' + data.education[k].logo + ')';
+            document.getElementsByClassName('job-center')[i].className += ' education-logo';
+            document.getElementsByClassName('job-center')[i].innerHTML = '';
+        }
+        else {
+            document.getElementsByClassName('job-center')[i].innerHTML = data.education[k].school; 
+            //document.getElementsByClassName('job-center')[i].className += ' job-company'; 
+        }
+        for (i=0; i < data.education[k].degrees.length; i++)  {
+            console.log(data.education[k].degrees.length);
+            document.getElementsByClassName('field')[i].innerHTML = data.education[k].degrees[i].name;  
+            document.getElementsByClassName('year')[i].innerHTML = data.education[k].degrees[i].date;    
+        }
+    }
 }
 
 function loadData() {
@@ -65,3 +69,6 @@ function loadData() {
     xhttp.open("GET", "data.json", true);
     xhttp.send();
 }
+
+// loadData();
+
